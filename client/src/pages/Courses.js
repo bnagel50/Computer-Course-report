9bf0c23662d8e7bc023ede7c9df8f89ddde9088b
Ivan Goya <ivan.goya98@gmail.com>
2022-07-17 13:36:11 -0400

    courses css work

--- client/src/pages/Courses.js
+++ client/src/pages/Courses.js
@@ -18,17 +18,22 @@ const Courses = () => {
     <main>
       <div className="flex-row justify-center">
         <div className="col-12 col-md-10 my-3">
+        <div>
+              <h1>hello</h1>
+        </div>
           {loading ? (
             <div>Loading...</div>
           ) : (
+            
             <div className="">
               {data.courses.map(course => (<div className="course-item card">
                 <p>Course {course.school}</p>
                 <p>Location: {course.location}</p>
                 <p>curriculum: {course.curriculum.join(', ')}</p>
+                <p>cost: ${course.cost}</p>
+                <p>length: {course.length}-weeks</p>
               </div>))}
             </div>
-
           )}
         </div>
       </div>
