c51a102fa8fdd4593e481499f754c6eae3f31ace
Nate LaBine <njlabine@gmail.com>
2022-07-18 16:29:08 -0400

    fixed the contact us email function

--- client/src/pages/ContactUs.js
+++ client/src/pages/ContactUs.js
@@ -1,7 +1,6 @@
 import React, { useRef } from "react";
-// Using emailjs to send me an email when someone fills out the form on the site
 import emailjs from "emailjs-com";
-import Form from 'react-bootstrap/Form';
+import Form from "react-bootstrap/Form";
 
 export default function Contact() {
   const form = useRef();
@@ -29,33 +28,53 @@ export default function Contact() {
 
   return (
     <main className="flex-row justify-center mb-4 main-log">
-       <div className="col-12 col-lg-10 first-div">
-        <div className='text-stuff'>
-           <h1>GET IN TOUCH WITH US 👇</h1>
-           <Form ref={form} onSubmit={sendEmail} id="contact-form">
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>Name</Form.Label>
-        <Form.Control size="lg" type="name" placeholder="Name"/>
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control size="lg" type="email" placeholder="Email" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Label>Tell us what Courses you Want us to Add! <br></br> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sapien ante, egestas bibendu</Form.Label>
-        <Form.Control as="textarea" rows={3} />
-      </Form.Group>
-      <button
-                  className="btn btn-block btn-info btn-submit"
-                  style={{ cursor: 'pointer' }}
-                  type="Let's Collaborate !"
-                >
-                  Let's Collaborate !
-                </button>
-    </Form>
+      <div className="col-12 col-lg-10 first-div">
+        <div className="text-stuff">
+          <h1>GET IN TOUCH WITH US 👇</h1>
+          <Form ref={form} onSubmit={sendEmail} id="contact-form">
+            <Form.Group
+              name="name"
+              className="mb-3"
+              controlId="exampleForm.ControlInput1"
+            >
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                name="from_name"
+                size="lg"
+                type="name"
+                placeholder="Name"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                name="email"
+                size="lg"
+                type="email"
+                placeholder="Email"
+              />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>
+                Tell us what Courses you Want us to Add! <br></br> Lorem ipsum
+                dolor sit amet, consectetur adipiscing elit. Aliquam sapien
+                ante, egestas bibendu
+              </Form.Label>
+              <Form.Control name="message" as="textarea" rows={3} />
+            </Form.Group>
+            <button
+              className="btn btn-block btn-info btn-submit"
+              style={{ cursor: "pointer" }}
+              type="Let's Collaborate !"
+            >
+              Let's Collaborate !
+            </button>
+          </Form>
+        </div>
       </div>
-    </div>
     </main>
   );
 }
-
