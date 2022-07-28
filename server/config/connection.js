aad68b4f33d580aaa4c7260077a9602a7076ef0c
bnagel <nagelb500@gmail.com>
2022-07-27 20:49:08 -0400

    connection fixes

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/no-bs-bc",
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/no-bs-bc",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
